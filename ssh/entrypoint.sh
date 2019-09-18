15a9012ac6d8979d7b14add447dae25e1c2c46ef
Hugo Demiglio <hugo.demiglio@bild.com.br>
2019-09-18 11:13:01 -0300

    Permissions on chmod

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -4,14 +4,16 @@ set -e
 
 SSH_PATH="$HOME/.ssh"
 
+echo "Starting connection to $HOST as $USER..."
+
 mkdir -p "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
 
 echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 
-chmod 700 "$SSH_PATH"
-chmod 600 "$SSH_PATH/known_hosts"
-chmod 600 "$SSH_PATH/deploy_key"
+sudo chmod 700 "$SSH_PATH"
+sudo chmod 600 "$SSH_PATH/known_hosts"
+sudo chmod 600 "$SSH_PATH/deploy_key"
 
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
